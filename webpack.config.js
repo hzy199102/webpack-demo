49e3acab1950e12051cf571a29955674c77121b1
hzy199102 <203161585@qq.com>
2018-05-29 00:02:10 +0800

    1.加入了可以查看manifest的插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
     entry: {
@@ -13,6 +14,7 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
+        new ManifestPlugin(),
              new HtmlWebpackPlugin({
                    title: 'Output Management'
      })
